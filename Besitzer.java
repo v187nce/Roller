d593276168fc434b17607ca962e0ccce484f93d8
Juliorocktect <113830349+Juliorocktect@users.noreply.github.com>
2023-05-16 12:48:11 +0200

    Create Besitzer.java

---
+++ Besitzer.java
@@ -0,0 +1,25 @@
+public class Besitzer{
+  private double geld;
+  private String name;
+  private Roller roller;
+  
+  public Besitzer(double geld, String name, Roller roller){
+    this.geld = geld;
+    this.name = name;
+    this.roller = roller;
+  }
+  public double getGeld(){
+    return geld;
+  }
+  public void FahreDeinenRoller(int strecke){
+    roller.fahre(strecke);
+  }
+  public void tankeDeinenRoller(double menge){
+    if (roller.passInTank(menge)){
+      roller.tanke(menge);
+    }
+  }
+  public void erhalteTaschenGeld(double summe){
+    geld = geld + summe;
+  }
+}
