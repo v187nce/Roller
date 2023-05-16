cc05ff28c882355b76255f5107e1672ee0ac75c9
Juliorocktect <113830349+Juliorocktect@users.noreply.github.com>
2023-05-16 12:40:05 +0200

    Create Roller.java

---
+++ Roller.java
@@ -0,0 +1,30 @@
+public class Roller{
+  private double tankgroesse;
+  private int kilometerstand;
+  private double tankinhalt;
+  
+  public Roller(double tankgroesse, int kilometerstand, double tankinhalt){
+    this.tankgroesse = tankgroesse;
+    this.kilometerstand = kilometerstand;
+    this.tankinhalt = tankinhalt;
+  }
+  
+  public void tanke(double menge){
+    tankinhalt = tankinhalt + mange;
+  }
+  public void fahre(int strecke){
+    kilometerstand = kilometerstand + strecke;
+  }
+  public boolean passtInTank(double menge){
+    if (tankgoesse > tankinhalt + menge){
+      return false;
+    }
+    return true;
+  }
+  public double getTankinhalt(){
+    return tankinhalt;
+  }
+  public int getKilometerstand(){
+    return kilometerstand;
+  }
+}
